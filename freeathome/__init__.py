d76f4b45e57247ba0d4662a9da1162882a7e6186
Lasse Bjørknes Magnussen <lasse.magnussen@vipps.no>
2019-02-25 18:28:16 +0100

    Moved consts to const.py

--- freeathome/__init__.py
+++ freeathome/__init__.py
@@ -1,19 +1,13 @@
 ''' Main Home Assistant interface Free@Home '''
-import logging
 import voluptuous as vol
 from homeassistant.helpers.discovery import load_platform
 from homeassistant.const import CONF_HOST, CONF_USERNAME, CONF_PASSWORD, CONF_PORT
 import homeassistant.helpers.config_validation as cv
 
+from .const import (DOMAIN, DATA_MFH, CONF_USE_ROOM_NAMES, DEFAULT_USE_ROOM_NAMES)
 from .pfreeathome import FreeAtHomeSysApp
 
 REQUIREMENTS = ['slixmpp==1.4.2']
-
-DOMAIN = 'freeathome'
-
-DATA_MFH = 'FAH'
-CONF_USE_ROOM_NAMES = 'use_room_names'
-DEFAULT_USE_ROOM_NAMES = False
 
 # Validation of the user's configuration
 CONFIG_SCHEMA = vol.Schema({
@@ -26,8 +20,6 @@ CONFIG_SCHEMA = vol.Schema({
                      default=DEFAULT_USE_ROOM_NAMES): cv.boolean,
     })
 }, extra=vol.ALLOW_EXTRA)
-
-_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup(hass, base_config):
---
+++ freeathome/const.py
@@ -0,0 +1,8 @@
+"""Constants for the Free@Home component."""
+import logging
+
+_LOGGER = logging.getLogger('custom_components.pfreeathome')
+DOMAIN = 'freeathome'
+DATA_MFH = 'FAH'
+CONF_USE_ROOM_NAMES = 'use_room_names'
+DEFAULT_USE_ROOM_NAMES = False
